af5836dfd0bc7d085036e3b896a6d012e4b29e2d
Elvina <elvina_zamaletdinova@mail.ru>
2022-05-11 10:54:09 +0300

    Добавьте файлы проекта.

---
+++ DemoEx/AddPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoEx
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPage.xaml
+    /// </summary>
+    public partial class AddPage : Page
+    {
+        private Agent _currentAgent = new Agent();
+        public AddPage( Agent selectedAgent)
+        {
+            InitializeComponent();
+            DataContext = _currentAgent;
+            CbType.ItemsSource = ExdemoEntities.GetContext().AgentType.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentAgent.Title))
+                errors.AppendLine("Укажите название компании!");
+            if (string.IsNullOrWhiteSpace(_currentAgent.Address))
+                errors.AppendLine("Укажите адрес!");
+            if (string.IsNullOrWhiteSpace(_currentAgent.INN))
+                errors.AppendLine("Укажите название ИНН!");
+            if (string.IsNullOrWhiteSpace(_currentAgent.KPP))
+                errors.AppendLine("Укажите название КПП!");
+            if (string.IsNullOrWhiteSpace(_currentAgent.DirectorName))
+                errors.AppendLine("Укажите ФИО директора!");
+            if (string.IsNullOrWhiteSpace(_currentAgent.Phone))
+                errors.AppendLine("Укажите номер телефона!");
+            if (string.IsNullOrWhiteSpace(_currentAgent.Email))
+                errors.AppendLine("Укажите email!");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentAgent.ID== 0)
+                ExdemoEntities.GetContext().Agent.Add(_currentAgent);
+            try
+            {
+                ExdemoEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ DemoEx/AgentPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoEx
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentPage.xaml
+    /// </summary>
+    public partial class AgentPage : Page
+    {
+       
+        public AgentPage()
+        {
+            InitializeComponent();
+            var _currentAgent = ExdemoEntities.GetContext().Agent.ToList();
+            LViewAgent.ItemsSource = _currentAgent;
+
+            var allTypes = ExdemoEntities.GetContext().AgentType.ToList();
+            allTypes.Insert(0, new AgentType
+            {
+                Title = "Все типы"
+            });
+
+            CbType.ItemsSource = allTypes;
+            CbType.SelectedIndex = 0;
+
+        }
+
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                ExdemoEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                LViewAgent.ItemsSource = ExdemoEntities.GetContext().Agent.ToList();
+            }
+        }
+
+       
+        private void CbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TxbPageNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void GoNextPageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GoPrevButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LViewAgent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddPage(null));
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            var agentsForRemoving = LViewAgent.SelectedItems.Cast<Agent>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {agentsForRemoving.Count()} элементов?","Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    ExdemoEntities.GetContext().Agent.RemoveRange(agentsForRemoving);
+                    ExdemoEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+
+                    LViewAgent.ItemsSource = ExdemoEntities.GetContext().Agent.ToList();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
